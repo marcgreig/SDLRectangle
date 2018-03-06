0388f2a8d1e67c8ce6304d42593926ed443bbdb5
marcgreig <36154273+marcgreig@users.noreply.github.com>
2018-03-06 09:46:35 +0100

    Fill Window with Color

--- SDLRect/Source.cpp
+++ SDLRect/Source.cpp
@@ -8,6 +8,12 @@ int main(int argc, char* argv[]) {
 	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, 0);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
+	while (1) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
