11ec5152b48aa59b348f2bb0afb7b605e97b641b
marcgreig <36154273+marcgreig@users.noreply.github.com>
2018-03-06 09:43:22 +0100

    Creat Window

--- SDLRect/Source.cpp
+++ SDLRect/Source.cpp
@@ -5,5 +5,9 @@
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 
+	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, 0);
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
+	return 0;
 }
