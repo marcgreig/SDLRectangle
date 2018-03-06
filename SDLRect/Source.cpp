ddbfefeb71d8bdfc9ead57c0e26bfff0f0417008
marcgreig <36154273+marcgreig@users.noreply.github.com>
2018-03-06 09:45:36 +0100

    Create Renderer

--- SDLRect/Source.cpp
+++ SDLRect/Source.cpp
@@ -6,7 +6,9 @@ int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
