0a5315b70601f93ff346606b595c15c1ceaca6ed
marcgreig <36154273+marcgreig@users.noreply.github.com>
2018-03-06 09:47:33 +0100

    Create Rectangle

--- SDLRect/Source.cpp
+++ SDLRect/Source.cpp
@@ -6,11 +6,18 @@ int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, 0);
+	
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	
+	SDL_Rect rect = { 550, 350, 200, 150 };
 
 	while (1) {
+		
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderClear(renderer);
+
+
+
 		SDL_RenderPresent(renderer);
 	}
 
